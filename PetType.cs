4d80426b4f72aa52569668748bda69a8b81dfae1
IrmakKaytan <irmak.kaytan@stu.istinye.edu.tr>
2025-05-20 19:56:50 +0300

    Pet and PetType classes added

---
+++ PetType.cs
@@ -0,0 +1,9 @@
+namespace DGD208_Spring2025_IrmakKaytan
+{
+    public enum PetType
+    {
+        MemoryEater,    // RAM consuming virus
+        FunMiner,       // CPU intensive virus
+        SleepCrawler    // Background process virus
+    }
+} 

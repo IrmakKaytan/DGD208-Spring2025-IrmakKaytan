87c73b6190122da7925d499663cef865c1940d6d
IrmakKaytan <irmak.kaytan@stu.istinye.edu.tr>
2025-05-27 17:49:06 +0300

    Delete PetType.cs

--- PetType.cs
+++
@@ -1,9 +0,0 @@
-namespace DGD208_Spring2025_IrmakKaytan
-{
-    public enum PetType
-    {
-        MemoryEater,    // RAM consuming virus
-        FunMiner,       // CPU intensive virus
-        SleepCrawler    // Background process virus
-    }
-} 
